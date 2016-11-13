17c600a1a7c57c13d018254ab51189649ada96d9
Walking Disaster <michaelmeadows@gmail.com>
2016-11-13 15:36:04 -0500

    Initial

---
+++ coazure/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace coazure.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ coazure/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="coazure.MvcApplication" Language="C#" %>
